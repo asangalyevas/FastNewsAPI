b2f4a8c4d8a553eb331eb02c7edaca16dff8eeb4
asangalyeva_s <sabinaasangalyeva9@gmail.com>
2025-02-04 14:04:03 +0600

    alembic migrations

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+# from fastapi import FastAPI
+# from pydantic import BaseModel
 
-app = FastAPI()
+# app = FastAPI()
 
-class News(BaseModel):
-    title: str
-    content: str | None = None
-    views: int = 0
+# class News(BaseModel):
+#     title: str
+#     content: str | None = None
+#     views: int = 0
 
-@app.post("/news/")
-async def create_news(news: News) -> News:
-    "endpoint to create news"
-    return news
+# @app.post("/news/")
+# async def create_news(news: News) -> News:
+#     "endpoint to create news"
+#     return news
 
 
 # @app.get("/news/{news_id}")
@@ -32,3 +32,10 @@ async def create_news(news: News) -> News:
 # async def root():
 #     return {"message": "Hello World!"}       
 
+"""
+Main frogramm module
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
